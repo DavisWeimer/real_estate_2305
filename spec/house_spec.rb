59fb2223d6712f007e17578005d3ea96bf5c5635
Davis Weimer <davisweimer@gmail.com>
2023-05-18 09:54:22 -0600

    spec: Adds passing address/rooms tests

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -12,7 +12,16 @@ RSpec.describe House do
       house = House.new("$400000", "123 sugar lane")
       expect(house.price).to eq(400000)
     end
-
+    
+    it "has an address" do
+      house = House.new("$400000", "123 sugar lane")
+      expect(house.address).to eq("123 sugar lane")
+    end
+    
+    it "can have rooms someday perhaps.." do
+      house = House.new("$400000", "123 sugar lane")
+      expect(house.rooms).to eq []
+    end
   end
 
 
