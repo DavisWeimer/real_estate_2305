ea5e94ccf48c37fbe6dcb0a1b6279fbc8365df53
Davis Weimer <davisweimer@gmail.com>
2023-05-18 16:18:55 -0600

    lib: Adds area, details, price_per_square_foot, & rooms_sorted_by_area methods

--- lib/house.rb
+++ lib/house.rb
@@ -1,14 +1,9 @@
 class House
-  attr_reader :address, :rooms
+  attr_reader :price, :address, :rooms
   def initialize(price, address)
-    @price = price
+    @price = price.delete("$").to_i
     @address = address
     @rooms = []
-  end
-
-  def price
-    @price.delete! "$"
-    @price.to_i
   end
 
   def add_room(room)
@@ -16,7 +11,7 @@ class House
   end
 
   def above_market_average?
-    @price.to_i > 500000
+    @price > 500000
   end
   
   def rooms_from_category(category)
@@ -30,10 +25,23 @@ class House
   def area
     house_area = []
     @rooms.each do |room|
-      house_area.push(room.length * room.width.to_i)
+      house_area.push(room.area)
     end
     house_area.sum
   end
 
+  def details
+    {"price" => @price, "address" => @address}
+  end
+
+  def price_per_square_foot
+    price.fdiv(area).round(2)
+  end
+
+  def rooms_sorted_by_area
+    @rooms.sort do |room|
+      room.area
+    end
+  end
 end
 
