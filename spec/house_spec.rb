2539406a27f03bf8a8b26fbccb852b390a057f52
Davis Weimer <davisweimer@gmail.com>
2023-05-18 16:19:25 -0600

    spec: Adds passing area, details, price_per_square_foot, & rooms_sorted_by_area tests

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -75,6 +75,45 @@ end
 
       expect(house.area).to eq(1900)
     end
-  end
 
+    it "stores atrributes in a hash" do
+      house = House.new("$400000", "123 sugar lane")
+
+      expect(house.details).to eq({"price" => 400000, "address" => "123 sugar lane"})
+    end
+  end
+end
+
+RSpec.describe House do
+  describe "Iteration 4" do
+    it "has a price per foot" do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+
+      expect(house.price_per_square_foot).to eq(210.53)
+    end
+
+    it "can sort rooms area greatest to least" do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+    
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+    
+      expect(house.rooms_sorted_by_area).to eq([room_4, room_3, room_2, room_1])
+    end
+  end
 end
